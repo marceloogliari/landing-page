1ac6c71a789348b36ca131373a01bea0c99cffc0
Marcelo Ogliari <marcelo.ogliari@gmail.com>
2022-08-23 13:00:05 -0300

    first commit

---
+++ src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,20 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  query GET_LANDING_PAGE {
+    landingPage {
+      data {
+        attributes {
+          logo {
+            data {
+              attributes {
+                alternativeText
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default GET_LANDING_PAGE
